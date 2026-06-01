3cd0d5831afae2e2873f3b0a679befa4796c9d16
KinyooZ <104127633+KinyooZ@users.noreply.github.com>
2026-06-01 16:39:40 +0800

    chore(icons): 发布 v0.2.9 (91 icons)

---
+++ src/colored/index.ts
@@ -0,0 +1,5 @@
+export { default as FileDoc } from './FileDoc';
+export { default as FilePptx } from './FilePptx';
+export { default as FileTxt } from './FileTxt';
+export { default as FileXls } from './FileXls';
+export { default as Folder } from './Folder';
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export * as Outlined from './outlined';
 export * as Filled from './filled';
+export * as Colored from './colored';
 
 export type { IconProps } from './types';
