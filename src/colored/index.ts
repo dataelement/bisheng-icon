5af9806726cf91d16926bfc23fde05a8a9573f00
KinyooZ <104127633+KinyooZ@users.noreply.github.com>
2026-06-02 11:04:05 +0800

    chore(icons): 发布 v0.2.10 (92 icons)

--- src/colored/index.ts
+++ src/colored/index.ts
@@ -1,4 +1,5 @@
 export { default as FileDoc } from './FileDoc';
+export { default as FileMd } from './FileMd';
 export { default as FilePptx } from './FilePptx';
 export { default as FileTxt } from './FileTxt';
 export { default as FileXls } from './FileXls';
